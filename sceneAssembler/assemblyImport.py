07797a8c755fd626f4482e1b7ddcafe1e21d17bd
MoshTD <fidel8a@gmail.com>
2014-03-04 23:17:02 -0300

    shader importer and assign

---
+++ sceneAssembler/assemblyImport.py
@@ -0,0 +1,42 @@
+import json
+import maya.cmds as cmds
+
+jsonPath="Y:/TRABAJOS/PECERA_MALVINAS/1_Shot_Related/_ASSETS_GENERALES/04_SHADING/1_PROYECTOS/ESPECIES/TONINAOVERA/shaders/data.json"
+
+def selection():
+   selObj=cmds.ls(sl=True)
+   attrObj=[]
+   for obj in selObj:
+       attrObj.append(cmds.getAttr("%s.xformName"%(obj)))
+       
+   return attrObj
+   
+   
+
+def importShaders(jsonFile):
+    
+    jsonFile=open(jsonFile)
+    jsonFile=json.load(jsonFile)
+    
+    #import shaders    
+    for shaders in jsonFile["materialPath"]:
+        
+        print shaders
+        
+def assignShaders(jsonFile,objName):
+    
+    jsonFile=open(jsonFile)
+    jsonFile=json.load(jsonFile)
+    for obj in objName:
+        print obj
+        for k,v in jsonFile[obj].iteritems():
+            print k, v
+            
+def assignAttr(jsonFile,objName):
+    
+    jsonFile=open(jsonFile)
+    jsonFile=json.load(jsonFile)
+    for obj in objName:
+        print obj
+        for k,v in jsonFile[obj].iteritems():
+            print k, v
