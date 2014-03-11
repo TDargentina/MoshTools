97520e2738581a211bdcf263c5f6f56e8d8c8fd8
TDargentina <fidel8a@gmail.com>
2014-03-11 17:53:31 -0300

    Revert "mas cambios"
    This reverts commit 8330ffd91b9de4cbb6a6105bd84f7764d908e273.

--- pipelineTools/assemblyTools/assemblyDataExport/main.py
+++ pipelineTools/assemblyTools/assemblyDataExport/main.py
@@ -5,6 +5,8 @@ from shiboken import wrapInstance
 from PySide import QtCore, QtGui
 from maya import OpenMayaUI as omui
 import maya.cmds as cmds
+import maya.mel as mel
+import json
 
 pathModule="C:/Users/fidel/Documents/GitHub/MoshTools/pipelineTools/"
 
@@ -31,12 +33,12 @@ ui_file = "C:/Users/fidel/Documents/GitHub/MoshTools/pipelineTools/assemblyTools
 form_class, base_class = tools.loadUiType(ui_file)
  
  
-class test(base_class, form_class):
+class shaderExporter(base_class, form_class):
  
     def __init__(self, parent):
         '''A custom window with a demo set of ui widgets'''
         # init our ui using the MayaWindow as parent
-        super(test, self).__init__(parent)
+        super(shaderExporter, self).__init__(parent)
         # uic adds a function to our class called setupUi, calling this creates
         # all the widgets from the .ui file
         self.setupUi(self)
@@ -57,9 +59,15 @@ class test(base_class, form_class):
 		self.close()
 		
     def processBtn(self):
-		self.attributes=assemblyDataExport_v002.getAtrributes()
-		self.materials=assemblyDataExport_v002.getMaterials()
-		print self.attributes
+        if self.shaderCheckBox.checkState() == QtCore.Qt.CheckState.Checked:
+            self.materials=assemblyDataExport_v002.getMaterials()            
+            self.saveFiles()
+        
+        if self.JsonCheckBox.checkState() == QtCore.Qt.CheckState.Checked:
+            
+		    self.attributes=assemblyDataExport_v002.getAtributes()
+		    #print assemblyDataExport_v002.getAtributes()
+		    self.saveConnections()
        
     def selectOutput(self):
         
@@ -73,12 +81,35 @@ class test(base_class, form_class):
     def pathOutput(self):
     	
     	mayaPath=assemblyDataExport_v002.getMayaSceneFolder()
-    	self.outputLineEdit.setText(mayaPath + "/" + ".json")
+    	#print mayaPath
+    	self.outputLineEdit.setText(mayaPath[0]+"/shaders/")
+    	
+    def saveFiles(self):
+        
+        mayaPath=assemblyDataExport_v002.getMayaSceneFolder()
+        mayaPath=mayaPath[1]
+        mayaPath=mayaPath.split(".") 
+        mel.eval("select -cl")
+                        
+        for shGr in self.materials:
+            mel.eval("select -tgl -ne %s"%(shGr))
+        cmds.file("%s/%s.ma" %(self.outputLineEdit.text(),mayaPath[0]+"_shader"), es=True, type="mayaAscii")
+        
+    def saveConnections(self):
+        
+        mayaPath=assemblyDataExport_v002.getMayaSceneFolder()
+        mayaPath=mayaPath[1]
+        mayaPath=mayaPath.split(".")      
+        with open('%s/%s.json'%(self.outputLineEdit.text(),mayaPath[0]+"_shader"), 'wb') as fp:
+            json.dump(self.attributes, fp)
+            
+        #print self.attributes
+        self.close()
         
         
 anchor = maya_main_window()
 for x in anchor.children():
-    if isinstance(x, test):
+    if isinstance(x, shaderExporter):
         x.close()
  
-test(anchor).show()
+shaderExporter(anchor).show()
